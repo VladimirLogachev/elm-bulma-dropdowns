f1ec28ceeedd933fd3a4f365a149c93d2541637d
Vladimir Logachev <logachev.dev@ya.ru>
2022-02-01 19:41:43 +0700

    Add data

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -6,4 +6,64 @@ import Html.Attributes exposing (..)
 
 main : Html.Html msg
 main =
-    h1 [ class "title" ] [ text "dropdowns" ]
+    div [ class "column" ]
+        [ section [ class "section" ]
+            [ h2 [ class "subtitle" ] [ text "Overall" ]
+            , span [] [ text "Compare" ]
+            , p [] [ text "Selected: ", text <| String.join ", " dataOverall ]
+            ]
+        ]
+
+
+dataOverall : List String
+dataOverall =
+    [ "Option 1"
+    , "Option 2"
+    , "Option 3"
+    , "Option 4"
+    , "Option 5"
+    , "Option 6"
+    , "Option 7"
+    , "Option 8"
+    , "Option 9"
+    , "Option 10"
+    ]
+
+
+dataCategory1 : List String
+dataCategory1 =
+    [ "Option 1"
+    , "Option 2"
+    , "Option 3"
+    , "Option 4"
+    , "Option 5"
+    , "Option 6"
+    , "Option 7"
+    , "Option 8"
+    , "Option 9"
+    ]
+
+
+dataCategory2 : List String
+dataCategory2 =
+    [ "Option 1"
+    , "Option 2"
+    , "Option 3"
+    , "Option 4"
+    , "Option 5"
+    , "Option 6"
+    , "Option 7"
+    , "Option 8"
+    ]
+
+
+dataCategory3 : List String
+dataCategory3 =
+    [ "Option 1"
+    , "Option 2"
+    , "Option 3"
+    , "Option 4"
+    , "Option 5"
+    , "Option 6"
+    , "Option 7"
+    ]
