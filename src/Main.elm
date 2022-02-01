e2c015a7a75ce45d93c289f8ee4667a461e65069
Vladimir Logachev <logachev.dev@ya.ru>
2022-02-01 19:52:45 +0700

    Add Option type

--- src/Main.elm
+++ src/Main.elm
@@ -14,64 +14,68 @@ main =
         ]
 
 
-viewOptions : List String -> Html msg
+viewOptions : List Option -> Html msg
 viewOptions options =
     section [ class "section" ]
         [ h2 [ class "subtitle" ] [ text "Overall" ]
         , span [] [ text "Compare" ]
-        , p [] [ text "Selected: ", text <| String.join ", " options ]
+        , p [] [ text "Selected: ", text <| String.join ", " <| List.map .title options ]
         ]
 
 
-dataOverall : List String
+type alias Option =
+    { id : String, title : String }
+
+
+dataOverall : List Option
 dataOverall =
-    [ "Option 1"
-    , "Option 2"
-    , "Option 3"
-    , "Option 4"
-    , "Option 5"
-    , "Option 6"
-    , "Option 7"
-    , "Option 8"
-    , "Option 9"
-    , "Option 10"
+    [ { id = "1", title = "Option 1" }
+    , { id = "2", title = "Option 2" }
+    , { id = "3", title = "Option 3" }
+    , { id = "4", title = "Option 4" }
+    , { id = "5", title = "Option 5" }
+    , { id = "6", title = "Option 6" }
+    , { id = "7", title = "Option 7" }
+    , { id = "8", title = "Option 8" }
+    , { id = "9", title = "Option 9" }
+    , { id = "10", title = "Option 10" }
     ]
 
 
-dataCategory1 : List String
+dataCategory1 : List Option
 dataCategory1 =
-    [ "Option 1"
-    , "Option 2"
-    , "Option 3"
-    , "Option 4"
-    , "Option 5"
-    , "Option 6"
-    , "Option 7"
-    , "Option 8"
-    , "Option 9"
+    [ { id = "1", title = "Option 1" }
+    , { id = "2", title = "Option 2" }
+    , { id = "3", title = "Option 3" }
+    , { id = "4", title = "Option 4" }
+    , { id = "5", title = "Option 5" }
+    , { id = "6", title = "Option 6" }
+    , { id = "7", title = "Option 7" }
+    , { id = "8", title = "Option 8" }
+    , { id = "9", title = "Option 9" }
     ]
 
 
-dataCategory2 : List String
+dataCategory2 : List Option
 dataCategory2 =
-    [ "Option 1"
-    , "Option 2"
-    , "Option 3"
-    , "Option 4"
-    , "Option 5"
-    , "Option 6"
-    , "Option 7"
-    , "Option 8"
+    [ { id = "1", title = "Option 1" }
+    , { id = "2", title = "Option 2" }
+    , { id = "3", title = "Option 3" }
+    , { id = "4", title = "Option 4" }
+    , { id = "5", title = "Option 5" }
+    , { id = "6", title = "Option 6" }
+    , { id = "7", title = "Option 7" }
+    , { id = "8", title = "Option 8" }
     ]
 
 
-dataCategory3 : List String
+dataCategory3 : List Option
 dataCategory3 =
-    [ "Option 1"
-    , "Option 2"
-    , "Option 3"
-    , "Option 4"
-    , "Option 5"
-    , "Option 6"
-    , "Option 7"
+    [ { id = "1", title = "Option 1" }
+    , { id = "2", title = "Option 2" }
+    , { id = "3", title = "Option 3" }
+    , { id = "4", title = "Option 4" }
+    , { id = "5", title = "Option 5" }
+    , { id = "6", title = "Option 6" }
+    , { id = "7", title = "Option 7" }
     ]
